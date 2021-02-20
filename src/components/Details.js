a462bacd260d0929e515f4322997d67535c55ab5
samsmoak i <samsmoaki@samsmoaks-MacBook-Pro.local>
2021-02-21 03:33:52 +0530

    My message

---
+++ src/components/Details.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { ProductConsumer } from "../Context";
+import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button";
+
+class Details extends Component {
+  render() {
+    return (
+      <ProductConsumer>
+        {(value) => {
+          const {
+            id,
+            company,
+            price,
+            img,
+            inCart,
+            title,
+            info,
+          } = value.detailProduct;
+          return (
+            <div className='container py-5'>
+              {/*title */}
+
+              <div className='row'>
+                <div className='col-10 mx-auto text-center text-slanted text-blue my-5'>
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              {/*tile end*/}
+              {/*product info */}
+              <div className='row'>
+                <div className='col-10 mx-auto col-md-6 my-3'>
+                  <img src={img} className='img-fluid' alt='product' />
+                </div>
+
+                {/*product text */}
+                <div className='col-10 mx-auto col-md-6 my-3 text-capitalize'>
+                  <h2>model:{title}</h2>
+
+                  <h4 className='text-title text-uppercase text-muted mt-3 mb-2'>
+                    made by:<span className='text-uppercase'>{company}</span>
+                  </h4>
+                  <h4 className='text-blue'>
+                    <strong>
+                      price:<span>$</span>
+                      {price}
+                    </strong>
+                  </h4>
+                  <p className='text-capitalise font-weight-bold mt-3 mb-0'>
+                    some info about product:
+                  </p>
+                  <p className='text-muted lead'> {info}</p>
+                  <div>
+                    <Link to='/'>
+                      <ButtonContainer>Back to Product </ButtonContainer>
+                    </Link>
+
+                    <ButtonContainer
+                      cart
+                      disabled={inCart ? true : false}
+                      onClick={() => {
+                        value.addToCart(id);
+                        value.openModal(id);
+                      }}
+                    >
+                      {inCart ? "incart" : "add to cart"}
+                    </ButtonContainer>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </ProductConsumer>
+    );
+  }
+}
+
+export default Details;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { ProductProvider } from "./Context";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ProductProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </ProductProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
